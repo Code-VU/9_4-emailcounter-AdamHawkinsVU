2666b897af5aef9fc6bc0aaf4768332a2c51b07a
Adam Hawkins <Adam.Hawkins@vu.com>
2022-03-29 12:38:13 -0500

    attempt #1

--- counter.py
+++ counter.py
@@ -3,9 +3,30 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+
+    lst = list()
+
+    for line in handle:
+        if not line.startswith("From:"): continue
+        line = line.split()
+        lst.append(line[1])
+
+    counts = dict()
+    for word in lst:
+        counts[word] = counts.get(word,0) +1
+
+    bigcount = None
+    bigword = None
+    
+    for word,count in counts.items():
+        if bigcount is None or count > bigcount:
+            bigcount=count
+            bigword = word 
+    
+    print (bigword,bigcount)
         
 ## if you want to test locally before you try to sync
 ## uncomment countEmail() and run > python counter.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-##countEmail()
+#countEmail()
